8f9887b9f24cb66417024bfb289d7b8bedae795a
JamesClow <james.c.clow@hollandhart.com>
2018-10-19 11:58:38 -0600

    added swiping and better styling.

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -19,12 +19,18 @@ export default{
     //     this.appElm.scrollTop = 0;
     //   }
     // }
+    var message_elm = document.getElementById("message-section");
+    if(message_elm){
+      message_elm.addEventListener('touchstart', this.handleTouchStart, false);
+      message_elm.addEventListener('touchmove', this.handleTouchMove, false);
+    }
   },
   data(){
     return {
       active_punch_index: 0,
       punch_loop: null,
       punch_delay: 2500,
+      active_product_index: 0,
       punches: [
         "Powerful.",
         "Simple.",
@@ -58,7 +64,8 @@ export default{
           color: "yellow",
         }
       ],
-      active_product_index: 0,
+      xDown: null,
+      yDown: null,
       product_loop: null,
       product_delay: 3000,
       product_callback: null,
@@ -204,6 +211,43 @@ export default{
     },
     goTo(route){
       this.$router.push(route)
+    },
+
+    ///////////////////////////// Swiping //////////////////////////////////////
+
+    getTouches(evt){
+      return evt.touches || evt.originalEvent.touches;
+    },
+    handleTouchStart(evt){
+      this.xDown = this.getTouches(evt)[0].clientX;
+      this.yDown = this.getTouches(evt)[0].clientY;
+    },
+    handleTouchMove(evt){
+      if( ! this.xDown || ! this.yDown ){
+        return;
+      }
+      var xUp = evt.touches[0].clientX;
+      var yUp = evt.touches[0].clientY;
+      var xDiff = this.xDown - xUp;
+      var yDiff = this.yDown - yUp;
+      if( Math.abs( xDiff ) > Math.abs( yDiff ) ){
+        if( xDiff > 0 ){
+          /* left swipe */
+          this.nextMessage(-1);
+        }else{
+          /* right swipe */
+          this.nextMessage(1);
+        }
+      }else{
+        if( yDiff > 0 ){
+          /* up swipe */
+        }else{
+          /* down swipe */
+        }
+      }
+      /* reset values */
+      this.xDown = null;
+      this.yDown = null;
     }
   }
 }
